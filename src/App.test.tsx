43506054a3eefd8b323aa4f8d738f236e744c4d1
Yordanys <yordyat9@gmail.com>
2022-06-21 10:25:56 -0400

    Project with lint-staged

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/leeeeearn react/i);
+  const linkElement = screen.getByText(/leearn react/i);
   expect(linkElement).toBeInTheDocument();
 });
