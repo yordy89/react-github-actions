fc9d75498efa3b69bd188b112151dbdee95b791c
Yordanys <yordyat9@gmail.com>
2022-06-21 10:37:10 -0400

    Project with lint-staged

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/leearn react/i);
+  const linkElement = screen.getByText(/leeeearn react/i);
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code>
-          and save to reload.
+          and savee to reload.
         </p>
         <a
           className="App-link"
